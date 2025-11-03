0e782ef89c1b437c67556b434c17c412bdd31db2
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-11-03 14:41:08 -0300

    Finalização

--- characters_environment.js
+++ characters_environment.js
@@ -213,6 +213,7 @@ function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
     coin.get=true;
+    mario_coin.play()
   };
 }
     
@@ -302,6 +303,8 @@ function manualControl(character){
 function jumping(character){
 	if( (noseY < 200 &&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
+    mario_jump.play();
+  
 	}
 }
 
@@ -356,6 +359,7 @@ function StepOnEnemy(obj1,obj2){
     }else{
       obj1.velocity.y+=gameConfig.jump*0.8;
     }
+    mario_kick.play();
 	}
 }
 
@@ -368,6 +372,9 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+    if(character.liveNumber > 0) {
+      mario_die.play();
+    }
 }
 
 // Verificar o status e resposta do personagem ao status do sprite e jogo
@@ -379,6 +386,7 @@ function checkStatus(character){
   }
   if(character.live==false && character.liveNumber==0){
     gameConfig.status="gameover"
+    mario_gameover.play();
   }
 
 }
